93a2ad1f2f74eda894deec24a6169cf839f775d3
Remy <insomnia128@hotmail.com>
2020-03-30 22:18:46 +0800

    Include code for post, pre and in order traversal

---
+++ 4_Trees_And_Graphs/Traversals/Pre_Order.js
@@ -0,0 +1,35 @@
+const Node = require("./Node");
+
+preOrder = (node) => {
+  if (!node) {
+    return;
+  }
+  console.log(node.val);
+  preOrder(node.left);
+  preOrder(node.right);
+}
+
+let node8 = new Node(8);
+let node3 = new Node(3);
+let node5 = new Node(5);
+let node10 = new Node(10);
+let node11 = new Node(11);
+let node6 = new Node(6);
+let node7 = new Node(7);
+
+node8.left = node3;
+node8.right = node5;
+node3.left = node10;
+node3.right = node11;
+node5.left = node6;
+node5.right = node7;
+
+/*
+          8
+        /   \
+      3       5
+    /   \   /   \
+   10   11  6    7    
+*/
+
+preOrder(node8);
